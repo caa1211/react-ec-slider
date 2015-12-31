93043a52d4e9db9f2c7cd971465fa5c35d1aa2b2
Carter Chang <carter@yahoo-inc.com>
2015-12-31 16:43:10 +0800

    first version

---
+++ ecSlider.js
@@ -0,0 +1,14 @@
+var EcSlider = React.createClass({
+	
+	  render: function () {
+	  	return (<div style={{border: 'solid 1px red'}}>aaaaaa</div>);
+	  }
+
+});
+
+
+React.render(
+    <EcSlider />
+    ,
+    document.body
+);
